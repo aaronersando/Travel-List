8a7c0e0080e38894bd8068e82dad189d0bf2b18d
aaronersando <agersando@gmail.com>
2025-06-21 17:07:31 +0800

    Item and item list setup done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,26 @@
 import { useState } from "react";
 
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
 
-  function handleSubmit() {}
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = {
+      description,
+      quantity,
+      packed: false,
+      id: new Date.now(),
+    };
+
+    onAddItems(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
