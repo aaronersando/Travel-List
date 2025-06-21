5ef61209fcf77dd5c0eb4191f9203f1e17b2ad6a
aaronersando <agersando@gmail.com>
2025-06-21 16:26:53 +0800

    Form for adding item to bring setup done

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,47 @@
-import { Logo } from "./Logo";
-import { Form } from "./Form";
-import { PackingList } from "./PackingList";
-import { Stats } from "./Stats";
+import Logo from "./Logo";
+import Form from "./Form";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
+import { useState } from "react";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItem(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) => {
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      );
+    });
+  }
+
+  function handleClearList() {
+    const confirmation = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+
+    if (confirmation) setItems([]);
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
-      <Stats />
+      <Form onAddItems={handleAddItem} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
+      />
+      <Stats items={items} />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,3 +1,33 @@
-function Form() {}
+import { useState } from "react";
+
+function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit() {}
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 15 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
 
 export default Form;
